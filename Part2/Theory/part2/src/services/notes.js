ef62e1f5757828468e49d8970ce10b2b510c7f3a
Yogesh Dollin <ydollin98@gmail.com>
2023-07-21 10:28:36 +0530

    Finish Part4 -> Deploying to internet: - Same origin policy and CORS Time: 20 min

--- Part2/Theory/part2/src/services/notes.js
+++ Part2/Theory/part2/src/services/notes.js
@@ -1,15 +1,10 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = 'http://localhost:3001/api/notes'
 
 const getAll = () =>{
-    const nonExisting = {
-        id: 10000,
-        content: 'This note is not saved to server',
-        important: true
-    }
     return axios
             .get(baseUrl)
-            .then(response => response.data.concat(nonExisting))
+            .then(response => response.data)
 }
 
 const create = newObject => {
