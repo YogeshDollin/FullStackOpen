fdb882e7b9eeab27995380fc1d5ba202db7892e9
Yogesh Dollin <ydollin98@gmail.com>
2023-08-03 10:21:35 +0530

    Finish exercises 4.1 - 4.2 Time: 1.5hour

---
+++ Part4/Exercises/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const blogsRouter = require('./controllers/blogs')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+mongoose.connect(config.MONGODB_URL)
+    .then( result => {
+        logger.info('connected to DB')
+    })
+    .catch(error => {
+        logger.error('error connecting to DB error: ', error.message);
+    })
+
+module.exports = app
