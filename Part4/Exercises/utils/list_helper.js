6735c9d162493e0787ac79115370774ee5af9c87
Yogesh Dollin <ydollin98@gmail.com>
2023-08-04 10:45:48 +0530

    Finish exercises 4.5 - 4.6 Time: 1 hour

--- Part4/Exercises/utils/list_helper.js
+++ Part4/Exercises/utils/list_helper.js
@@ -9,7 +9,62 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    let mostLiked = {likes: 0}
+    let favoriteBlogs = []
+    blogs.forEach(blog => {
+        if(mostLiked.likes < blog.likes){
+            mostLiked = blog
+        }
+        else if(mostLiked.likes === blog.likes){
+            favoriteBlogs.push(mostLiked)
+            mostLiked = blog
+        }
+    });
+    favoriteBlogs.push(mostLiked)
+    return !mostLiked.title ? {} : {
+        title: favoriteBlogs[0].title,
+        author: favoriteBlogs[0].author,
+        likes: favoriteBlogs[0].likes
+    }
+}
+
+const authorBlogsDictionary = (blogs) => {
+    const dictionary = {}
+    blogs.forEach(blog => {
+        const author = blog.author
+        if(dictionary[author]){
+            dictionary[author].push(blog)
+        }else{
+            dictionary[author] = [blog]
+        }
+    });
+    return dictionary
+}
+
+const mostBlogs = (blogs) => {
+    let mostBlogs = {blogs: 0}
+    const authorsWithMostBlogs = []
+
+    const dict = authorBlogsDictionary(blogs)
+
+    for(author in dict){
+        const blogCount = dict[author].length
+        if(mostBlogs.blogs < blogCount){
+            mostBlogs = {author: author, blogs: blogCount}
+        }
+        else if (mostBlogs.blogs === blogCount){
+            authorsWithMostBlogs.push(mostBlogs)
+            mostBlogs = {author: author, blogs: blogCount}
+        }
+    }
+    authorsWithMostBlogs.push(mostBlogs)
+    return !mostBlogs.hasOwnProperty('author') ? {} : authorsWithMostBlogs[0]
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
