c5ba2663e288ba1d954918fc96e2e06af904edf2
Yogesh Dollin <ydollin98@gmail.com>
2023-08-15 22:31:47 +0530

    Finish exercise 4.21 Time: 25 min

--- Part4/Exercises/controllers/blogs.js
+++ Part4/Exercises/controllers/blogs.js
@@ -33,8 +33,20 @@ blogsRouter.post('/', async (request, response, next) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-    await Blog.findByIdAndRemove(request.params.id)
-    response.status(204).end()
+    const blogId = request.params.id
+    const decodedToken = jwt.verify(request.token, config.SECRET)
+
+    const blog = await Blog.findById(blogId)
+    const user = await User.findById(decodedToken.id)
+
+    if(blog && user && (blog.user.toString() === user._id.toString())){
+        await Blog.findByIdAndRemove(blog.id)
+        user.blogs = user.blogs.filter(b => b._id.toString() !== blog.id)
+        await user.save()
+        response.status(204).end()
+    }else{
+        response.status(401).json({error: "token invalid"})
+    }
 })
 
 blogsRouter.put('/:id', async (request, response) => {
