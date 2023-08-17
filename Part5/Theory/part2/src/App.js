3b54a3aacae41248e8c0a9754f10d642ca8d37cc
Yogesh Dollin <ydollin98@gmail.com>
2023-08-17 22:27:56 +0530

    Complete - Part5 - Login in frontend - Handling login - Creating new token Time: 30 min

---
+++ Part5/Theory/part2/src/App.js
@@ -0,0 +1,122 @@
+import {useState, useEffect} from 'react'
+import Note from './component/Note'
+import Notification from './component/Notification'
+import noteService from './services/notes'
+import Footer from './component/Footer'
+import loginServcie from './services/login'
+
+const App = (props) => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const notesToShow =  showAll ? notes : notes.filter( note => note.important)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initalNotes => {
+        setNotes(initalNotes)
+      })
+  }, [])
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(n => n.id === id ? returnedNote : n))
+      })
+      .catch(error => {
+        setErrorMessage(`the note '${note.content}' was already deleted from server`)
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const addNotes = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5
+    }
+    noteService
+      .create(noteObject)
+      .then(returnedNote =>{
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+  const handleChange = evt =>{
+    setNewNote(evt.currentTarget.value)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginServcie.login({username, password})
+
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input type='text' value={username} name='username' onChange={({target}) => setUsername(target.value)} />
+      </div>
+      <div>
+        password
+        <input type='password' value={password} name='password' onChange={({target}) => setPassword(target.value)}/>
+      </div>
+      <button type='submit'>Login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNotes}>
+      <input value={newNote} onChange={handleChange}/>
+      <button type="Submit">save</button>
+    </form>
+  )
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage}/>
+      {user === null ? loginForm() : noteForm()}
+      {
+        user && <div>
+          <p>{user.name} logged in</p>
+          </div>
+      }
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note => <Note key={note.id} note = {note} toggleImportance={() => toggleImportanceOf(note.id)}/>)}
+      </ul>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ Part5/Theory/part2/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App notes/>
+)
+
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
