7f633323d4772538118244dfa6530d39c175c70e
Yogesh Dollin <ydollin98@gmail.com>
2023-08-21 10:46:14 +0530

    Finish - exercise 5.7. Time: 30 min

--- Part5/Exercise/blog/src/components/Blog.js
+++ Part5/Exercise/blog/src/components/Blog.js
@@ -1,7 +1,26 @@
+import { useState } from "react"
+
 const Blog = ({blog}) => {
+    const [showContent, setShowContent] = useState(false)
+
+    const toggleShowContent = () => {
+        setShowContent(!showContent)
+    }
     return (
-        <div>
-            {blog.title} {blog.author}
+        <div className="blog">
+            {showContent ?
+                <div>
+                    {blog.title} <button onClick={toggleShowContent}>hide</button>
+                    <br/>
+                    {blog.url}
+                    <br/>
+                    likes {blog.likes}<button>like</button>
+                    <br/>
+                    {blog.author}
+                </div>
+                :
+                <div>{blog.title} {blog.author} <button onClick={toggleShowContent}>view</button></div>
+            }
         </div>
     )
 }
