856d367f4c40f4c4daa61665cbbf05ed6a6faa97
Yogesh Dollin <ydollin98@gmail.com>
2023-09-14 16:44:21 +0530

    Finish exercise 5.15. Time: 5 min

--- Part5/Exercise/blog/src/components/Blog.test.js
+++ Part5/Exercise/blog/src/components/Blog.test.js
@@ -39,3 +39,25 @@ test('clicking the button display blog url and likes', async () => {
     expect(container).toHaveTextContent('sample.com')
     expect(container).toHaveTextContent(`likes ${blog.likes}`)
 })
+
+test('clicking the remove button calls the handler', async () => {
+    const blog = {
+        title: 'Sample',
+        author: 'Anony',
+        likes: 12,
+        url:'sample.com'
+    }
+
+    const mockhandler = jest.fn()
+    const {container} = render(<Blog blog={blog} removeBlog={mockhandler}/>)
+
+    const user = userEvent.setup()
+
+    let button = screen.getByText('view')
+    await user.click(button)
+
+    button = screen.getByText('remove')
+    await user.click(button)
+
+    expect(mockhandler.mock.calls).toHaveLength(1)
+})
