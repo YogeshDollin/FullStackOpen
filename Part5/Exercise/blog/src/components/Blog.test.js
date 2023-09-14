af58973d4a12426d925ec4ceec82ac014bfa63a2
Yogesh Dollin <ydollin98@gmail.com>
2023-09-14 16:36:47 +0530

    Finish exercise 5.14. Time: 20 min

--- Part5/Exercise/blog/src/components/Blog.test.js
+++ Part5/Exercise/blog/src/components/Blog.test.js
@@ -1,5 +1,6 @@
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('render blog without details', () => {
@@ -19,4 +20,22 @@ test('render blog without details', () => {
 
     expect(element).not.toHaveTextContent('sample.com')
     expect(element).not.toHaveTextContent(`likes ${blog.likes}`)
+})
+
+test('clicking the button display blog url and likes', async () => {
+    const blog = {
+        title: 'Sample',
+        author: 'Anony',
+        likes: 12,
+        url:'sample.com'
+    }
+    const mockhandler = jest.fn()
+    const {container} = render(<Blog blog={blog} removeBlog={mockhandler}/>)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    expect(container).toHaveTextContent('sample.com')
+    expect(container).toHaveTextContent(`likes ${blog.likes}`)
 })
