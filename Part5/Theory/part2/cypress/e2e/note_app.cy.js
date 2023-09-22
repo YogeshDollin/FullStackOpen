5f3c737b976ecdd1f54506eab7b4b5ac8f62c00e
Yogesh Dollin <ydollin98@gmail.com>
2023-09-22 21:58:48 +0530

    Complete - Part5 - End to End testing - Controlling the state of the database (complete). - Failed login test - Bypassing the UI - Changing the importance of a note - Running and debugging the tests.

--- Part5/Theory/part2/cypress/e2e/note_app.cy.js
+++ Part5/Theory/part2/cypress/e2e/note_app.cy.js
@@ -1,14 +1,14 @@
 describe('Note app', () => {
 
   beforeEach(function(){
-    cy.request('POST', 'http://localhost:3000/api/testing/reset')
+    cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
     const user = {
       name: 'Tester',
       username: 'test',
       password: 'test'
     }
-    cy.request('POST', 'http://localhost:3000/api/users/', user)
-    cy.visit('http://localhost:3000')
+    cy.request('POST', `${Cypress.env('BACKEND')}/users/`, user)
+    cy.visit('')
   })
 
   it('front page can be opened', () => {
@@ -29,12 +29,34 @@ describe('Note app', () => {
     cy.contains('Tester logged in')
   })
 
+  it('login fails with wrong password', () => {
+    cy.contains('login').click()
+    cy.get('#username').type('test')
+    cy.get('#password').type('wrong')
+    cy.get('#login-submit').click()
+
+    cy.get('.error')
+      .should('contain', 'Wrong credentials')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+
+    cy.get('html').should('not.contain', 'Tester logged in')
+  })
+
   describe('when logged in', function() {
     beforeEach(function() {
-      cy.contains('login').click()
-      cy.get('#username').type('test')
-      cy.get('#password').type('test')
-      cy.get('#login-submit').click()
+      // cy.contains('login').click()
+      // cy.get('#username').type('test')
+      // cy.get('#password').type('test')
+      // cy.get('#login-submit').click()
+      // cy.request('POST', 'http://localhost:3000/api/login', {
+      //   username: 'test', password: 'test'
+      // }).then(response => {
+      //   localStorage.setItem('loggedNoteappUser', JSON.stringify(response.body))
+      //   cy.visit('http://localhost:3000/')
+      // })
+
+      cy.login({ username: 'test', password: 'test' })
     })
 
     it('a new note can be created', function() {
@@ -44,20 +66,36 @@ describe('Note app', () => {
       cy.contains('a note created by cypress')
     })
 
-    describe('and a note exists', function() {
+    describe('and several note exists', function() {
       beforeEach('and a note exists', function() {
-        cy.contains('new note').click()
-        cy.get('input').type('another note cypress')
-        cy.contains('save').click()
+        // cy.contains('new note').click()
+        // cy.get('input').type('another note cypress')
+        // cy.contains('save').click()
+        cy.createNote({
+          content: 'first note',
+          important: true
+        })
+
+        cy.createNote({
+          content: 'second note',
+          important: true
+        })
+
+        cy.createNote({
+          content: 'third note',
+          important: true
+        })
       })
 
       it('it can be made not important', function() {
-        cy.contains('another note cypress')
+        cy.contains('second note')
           .contains('make not important')
           .click()
 
-        cy.contains('another note cypress')
-          .contains('make not important')
+        cy.contains('show all').click()
+
+        cy.contains('second note')
+          .contains('make important')
       })
     })
   })
