8e31bede423a9e41c12d8c7675fe19076c5d89de
Yogesh Dollin <ydollin98@gmail.com>
2023-09-25 21:54:55 +0530

    Finish exercise 5.18. Time: 15 min

--- Part5/Exercise/blog/cypress/e2e/blog-app.cy.js
+++ Part5/Exercise/blog/cypress/e2e/blog-app.cy.js
@@ -1,6 +1,12 @@
 describe('Blog app', () => {
   beforeEach(() => {
     cy.request('POST','http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Tester',
+      username: 'test',
+      password: 'test'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
     cy.visit('http://localhost:3000/')
   })
 
@@ -8,5 +14,25 @@ describe('Blog app', () => {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login', () => {
+    it('succeeds with correct credentials', () => {
+      cy.get('#username').type('test')
+      cy.get('#password').type('test')
+      cy.get('#login-submit').click()
+
+      cy.contains('Tester logged in')
+    })
+
+    it('fails with incorect crendetials', () => {
+      cy.get('#username').type('test')
+      cy.get('#password').type('tset')
+      cy.get('#login-submit').click()
+
+      cy.get('#errorMessage')
+        .should('contain', 'Wrong credentials')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
