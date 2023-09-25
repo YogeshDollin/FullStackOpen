f45100615a3e9089674c643147e7b6d0d2532ee9
Yogesh Dollin <ydollin98@gmail.com>
2023-09-25 22:30:54 +0530

    Finish exercise 5.19. Time: 30 min.

--- Part5/Exercise/blog/cypress/e2e/blog-app.cy.js
+++ Part5/Exercise/blog/cypress/e2e/blog-app.cy.js
@@ -34,5 +34,28 @@ describe('Blog app', () => {
         .should('contain', 'Wrong credentials')
         .and('have.css', 'color', 'rgb(255, 0, 0)')
     })
+
+    describe('when logged in', () => {
+      beforeEach(() => {
+        // cy.get('#username').type('test')
+        // cy.get('#password').type('test')
+        // cy.get('#login-submit').click()
+
+        cy.login({username: 'test', password: 'test'})
+      })
+
+      it.only('a blog can be created',() => {
+        cy.contains('create new note').click()
+
+        cy.get('input[name="title"]').type('sample')
+        cy.get('input[name="author"').type('anonymous')
+        cy.get('input[name="url"]').type('sample.com')
+        cy.get('button[type="submit"]').click()
+
+        cy.get('#notification').contains('a new blog sample by anonymous')
+        cy.get('.blog')
+          .should('contain', 'sample anonymous')
+      })
+    })
   })
 })
