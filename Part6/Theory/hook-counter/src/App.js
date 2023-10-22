de62171916e3223262e54b89fff0c2aab6f4799d
Yogesh Dollin <ydollin98@gmail.com>
2023-10-22 20:45:06 +0530

    Complete - part6 - React Query, useReducer and the context - Defining the counter context in a separate file. Time: 30min. Acc Time: 13hour 5min.

--- Part6/Theory/hook-counter/src/App.js
+++ Part6/Theory/hook-counter/src/App.js
@@ -1,41 +1,15 @@
-import { useContext, useReducer } from 'react'
-import CounterContext from './CounterContext'
+import Display from './Components/Display'
+import Button from './Components/Button'
 
-const Display = () => {
-  const [counter, dispatch] = useContext(CounterContext)
-  return <div>{counter}</div>
-}
-
-const Button = ({type, label}) => {
-  const [counter, dispatch] = useContext(CounterContext)
-  return <button onClick={() => dispatch({type})} >{label}</button>
-}
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case "INC":
-        return state + 1
-    case "DEC":
-        return state - 1
-    case "ZERO":
-        return 0
-    default:
-        return state
-  }
-}
-
-const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0)
-
-  return (
-    <CounterContext.Provider value={[counter, counterDispatch]}>
+const App = () => {return (
+    <div>
       <Display/>
       <div>
         <Button type={'INC'} label='+'/>
         <Button type={'DEC'} label='-'/>
         <Button type={'ZERO'} label='0'/>
       </div>
-    </CounterContext.Provider>
+    </div>
   )
 }
 
