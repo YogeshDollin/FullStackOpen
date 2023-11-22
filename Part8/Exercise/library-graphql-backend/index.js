951196ff1fb2b52977bfb1ca6006f96445d4ec5c
Yogesh Dollin <ydollin98@gmail.com>
2023-11-22 09:30:39 +0530

    Finish exercise 8.3. Time: 5min Acc Time: 2hour 10min.

--- Part8/Exercise/library-graphql-backend/index.js
+++ Part8/Exercise/library-graphql-backend/index.js
@@ -105,10 +105,15 @@ const typeDefs = `
         id: ID!
         genres: [String!]!
     }
+    type Author {
+        name: String!
+        bookCount: Int!
+    }
     type Query {
         bookCount: Int!
         authorCount: Int!
         allBooks: [Book!]!
+        allAuthors: [Author!]!
     }
 `
 
@@ -116,7 +121,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks : () => books
+    allBooks : () => books,
+    allAuthors: () => authors.map(author => {
+        return {
+            ...author,
+            bookCount: books.filter(book => book.author === author.name).length
+        }
+    })
   }
 }
 
