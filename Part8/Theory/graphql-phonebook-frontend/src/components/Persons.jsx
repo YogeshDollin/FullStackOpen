f09f7db9e8e5ac8c6c801a06312f8f211dc9fc98
Yogesh Dollin <ydollin98@gmail.com>
2023-12-01 22:36:13 +0530

    Complete - part8 - React and GraphQL - Apollo Client - Making queries - Named queries and variables. Time: 1hour 20min. Acc time: 4hour 20min

---
+++ Part8/Theory/graphql-phonebook-frontend/src/components/Persons.jsx
@@ -0,0 +1,46 @@
+import { gql, useQuery } from "@apollo/client"
+import { useState } from "react"
+import Person from "./Person"
+
+const FIND_PERSON = gql`
+    query findPersonByName($nameToSearch: String!) {
+        findPerson(name: $nameToSearch) {
+            name
+            phone
+            id
+            address {
+                street
+                city
+            }
+        }
+    }
+`
+
+const Persons = ({persons}) => {
+    const [nameToSearch, setNameToSearch] = useState(null)
+    const result = useQuery(FIND_PERSON, {
+        variables: {
+            nameToSearch
+        },
+        skip: !nameToSearch
+    })
+
+    if(nameToSearch && result.data){
+        return (
+            <Person person={result.data.findPerson} onClose={() => setNameToSearch(null)}/>
+        )
+    }
+
+    if(!persons) return null
+
+    return (
+        <div>
+            {persons.map(p => (<div key={p.id}>
+                {p.name} {p.phone}
+                <button onClick={() => setNameToSearch(p.name)}>Show person</button>
+            </div>))}
+        </div>
+    )
+}
+
+export default Persons
