6747ce6431d4563b5d24ecb1a31494217e199fed
Yogesh Dollin <ydollin98@gmail.com>
2024-01-06 19:44:05 +0530

    Complete - part9 - Background and Introduction, First step with Typescript. - Setting things up. - Creating your first own types. - type narrowing - @types/{npm_package} - Improving the project - The alternative array syntax
    Time: 1hour 10min.
    Acc Time: 1hour 10min.

---
+++ Part9/Theory/multiplier.ts
@@ -0,0 +1,34 @@
+interface MultiplyValues {
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (argv: string[]): MultiplyValues => {
+    if(argv.length > 4) throw new Error('Too many arguments');
+    if(argv.length < 4) throw new Error('Not enough arguments');
+
+    if(!isNaN(Number(argv[2])) && !isNaN(Number(argv[3]))){
+        return {
+            value1: Number(argv[2]),
+            value2: Number(argv[3])
+        }
+    }else{
+        throw new Error('Provided values were not number')
+    }
+}
+
+const multiplicator = (a: number, b: number, printText: string) => {
+    console.log(printText, a * b);
+}
+
+try {
+    const {value1, value2} = parseArguments(process.argv)
+    multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, result is: `)
+} catch (error) {
+    let errorMessage = 'Something went wrong'
+    if(error instanceof Error){
+        errorMessage += 'Error: ' + error.message
+    }
+    console.log(errorMessage);
+    
+}
