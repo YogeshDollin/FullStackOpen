9cbc019fa940f3960be461f63e72d59a7c934a76
Yogesh Dollin <ydollin98@gmail.com>
2024-01-07 11:28:35 +0530

    Finish exercise 9.5. Time: 15min. Acc Time: 3hour.

--- Part9/Exercises/bmiCalculator.ts
+++ Part9/Exercises/bmiCalculator.ts
@@ -1,37 +1,39 @@
-interface HeightWeight {
-    height: number;
-    weight: number;
-}
-const parseArguments = (args: string[]): HeightWeight => {
-    if(args.length < 4) throw new Error('Not enough arguments')
-    if(args.length > 4) throw new Error('too many arguments')
+// interface HeightWeight {
+//     height: number;
+//     weight: number;
+// }
+// const parseArguments = (args: string[]): HeightWeight => {
+//     if(args.length < 4) throw new Error('Not enough arguments')
+//     if(args.length > 4) throw new Error('too many arguments')
 
-    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
-        return {
-            height: Number(args[2]),
-            weight: Number(args[3])
-        }
-    }
-    throw new Error('Provided values are not number')
-}
+//     if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))){
+//         return {
+//             height: Number(args[2]),
+//             weight: Number(args[3])
+//         }
+//     }
+//     throw new Error('Provided values are not number')
+// }
 
-const calculateBmi = (height: number, weight: number) => {
+const calculateBmi = (height: number, weight: number): {weight: number, height: number, bmi: string} => {
     const heightInMeters = height / 100
     const bmi = weight / (heightInMeters * heightInMeters)
+    const result = {height, weight, bmi: ''}
 
     if(bmi < 18.5){
-        console.log('Underweight (Unhealthy weight)');        
+        result.bmi = 'Underweight (Unhealthy weight)';
     }else if(bmi >= 18.5 && bmi < 23){
-        console.log('Normal (healthy weight)');        
+        result.bmi = 'Normal (healthy weight)'
     }else if(bmi >= 23 && bmi < 25 ){
-        console.log('Overweight I (At risk weight)');        
+        result.bmi = 'Overweight I (At risk weight)'
     }else if(bmi >= 25 && bmi < 30){
-        console.log('Overweight II (Moderately obese weight)');        
+        result.bmi = 'Overweight II (Moderately obese weight)'
     }else if (bmi >= 30){
-        console.log('Overweight III (Severly obese weight)');        
+        result.bmi = 'Overweight III (Severly obese weight)'
     }
+    return result
 }
-const {height, weight} = parseArguments(process.argv)
-calculateBmi(height, weight);
+// const {height, weight} = parseArguments(process.argv)
+// calculateBmi(height, weight);
 
-export default 'this is masking warning'
+export default calculateBmi
--- Part9/Exercises/index.ts
+++ Part9/Exercises/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import calculateBmi from "./bmiCalculator";
 const app = express();
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!!!')
 })
+
+app.get('/bmi', (req, res) => {
+    const height = req.query.height
+    const weight = req.query.weight
+    
+    if(!isNaN(Number(height)) && !isNaN(Number(weight))){
+        res.json(calculateBmi(Number(height), Number(weight)))
+    }else{
+        throw new Error('Malformed parameters')
+    }
+})
+
 const PORT=3003;
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);    
