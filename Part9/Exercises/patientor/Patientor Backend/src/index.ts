55f6c33f5ee9e1ef4697f5a2e2fa703404394d8c
Yogesh Dollin <ydollin98@gmail.com>
2024-01-08 21:30:47 +0530

    Finish exercise 9.10. Time: 10min. Acc Time: 5hour 10min.

--- Part9/Exercises/patientor/Patientor Backend/src/index.ts
+++ Part9/Exercises/patientor/Patientor Backend/src/index.ts
@@ -1,3 +1,5 @@
+import diagnosesData from "./data/diagnoses";
+
 const express = require('express');
 const cors = require('cors')
 const app = express();
@@ -9,6 +11,10 @@ const PORT = 3001;
 app.get('/api/ping', (_req: any, res: { send: (arg0: string) => void; }) => {
     res.send('pong');
 });
+
+app.get('/api/diagnoses', (_req: any, res: any) => {
+    res.send(diagnosesData)
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);    
